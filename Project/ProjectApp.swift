5748b0c38e52eda6eb8bc2bfedeff4aa2fbefd6e
jasonajordan <jasonajordan0@gmail.com>
2024-11-08 18:41:06 -0500

    Initial Commit

---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by Jason Jordan on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
